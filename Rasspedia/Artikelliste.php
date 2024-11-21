b7620c0d451518067cff1e39ce83786bddbd1230
juma-Ju <feuerherz.mine@gmail.com>
2024-11-21 10:28:43 +0100

    made the styling uniform on all pages

--- Rasspedia/Artikelliste.php
+++ Rasspedia/Artikelliste.php
@@ -39,24 +39,14 @@ $conn->close();
       <h2>Kategorien</h2>
       <ul>
         <li><a href="#">Anti-Schwarzer Rassismus</a></li>
-        <span class="spanline">---------------------------------------</span>
         <li><a href="#">Anti-asiatischer Rassismus</a></li>
-        <span class="spanline">---------------------------------------</span>
         <li><a href="#">Antisemitismus</a></li>
-        <span class="spanline">---------------------------------------</span>
         <li><a href="#">Antimuslimischer Rassismus</a></li>
-        <span class="spanline">---------------------------------------</span>
         <li><a href="#">Anti-Indigener Rassismus</a></li>
-        <span class="spanline">---------------------------------------</span>
         <li><a href="#">Anti-Roma-Rassismus (Antiziganismus)</a></li>
-        <span class="spanline">---------------------------------------</span>
         <li><a href="#">Rassismus gegenüber Menschen aus dem Nahen und Mittleren Osten</a></li>
-        <span class="spanline">---------------------------------------</span>
         <li><a href="#">Rassismus gegen lateinamerikanische Menschen</a></li>
-        <span class="spanline">---------------------------------------</span>
         <li><a href="#">Rassismus gegenüber anderen Gruppen</a></li>
-        <span class="spanline">---------------------------------------</span>
-        <li><a href="#">Gallerie</a></li>
       </ul>
     </aside>
 
@@ -65,10 +55,10 @@ $conn->close();
 
       <!-- Navigation -->
       <nav>
-      <a href="#">Hauptseite</a>
-      <a href="#">Über Rassismus</a>
-      <a href="#">Alle Artikel</a>
-      <a href="#">Kontakt</a>
+        <a href="index.html">Hauptseite</a>
+        <a href="#">Über Rassismus XXX</a>
+        <a href="artikelliste.php">Alle Artikel</a>
+        <a href="#">Kontakt XXX</a>
       </nav>
 
       <!-- Content Area -->
@@ -111,29 +101,24 @@ $conn->close();
       <button><a href="#footer">Scroll</a></button>
     </div> -->
     <footer id="footer">
-        <div class="col col1">
-            <h3>Rasspedia</h3>
-            <p>Made with <span style="color: #BA6573;">❤ </span>by E1IT1</p>
-            <div class="social">
-            <a href="https://codepen.io/Juxtopposed" target="_blank" class="link"><img src="https://assets.codepen.io/9051928/codepen_1.png" alt="" /></a>
-            <a href="https://twitter.com/juxtopposed" target="_blank" class="link"><img src="https://assets.codepen.io/9051928/x.png" alt="" /></a>
-            <a href="https://youtube.com/@juxtopposed" target="_blank" class="link"><img src="https://assets.codepen.io/9051928/youtube_1.png" alt="" /></a>
-            </div>
-            <p style="color: #818181; font-size: smaller">2024 © All Rights Reserved</p>
+      <div class="col col1">
+        <h3>Rasspedia</h3>
+        <p>Made with <span style="color: #BA6573;">❤</span> by E1IT1</p>
+        <div class="social">
+
         </div>
-        <div class="col col2">
-            <p>About</p>
-            <p>Our mission</p>
-            <p>Privacy Policy</p>
-            <p>Terms of service</p>
-        </div>
-        <div class="col col3">
-            <p>Services</p>
-            <p>Products</p>
-            <p>Join our team</p>
-            <p>Partner with us</p>
-        </div>
-        <div class="backdrop"></div>
+        <p style="color: #818181; font-size: smaller">2024 © All Rights Reserved</p>
+      </div>
+      <div class="col col2">
+        <p>Über Uns</p>
+        <p>Unsere Mission</p>
+        <p>Datenschutzerklärung</p>
+        <p>Impressum</p>
+      </div>
+      <div class="col col3">
+        <h1>Rasspedia</h1>
+      </div>
+      <div class="backdrop"></div>
     </footer>
   </body>
 </html>
